1c1809f88ff8a27cf6cfb7e9e150cb635dde8a2f
Sammy <sammy.adjones@gmail.com>
2025-05-12 10:03:42 -0400

    Just tried adding page lol

---
+++ app/(app)/studenthome/python/page.tsx
@@ -0,0 +1,14 @@
+export default function Page() {
+    return (
+        <>
+        <button id="thebutton">
+            Make a new project here
+            alert('hello');
+        </button>
+
+        <div>
+            eh put whatever here
+        </div>
+        </>
+    );
+}
