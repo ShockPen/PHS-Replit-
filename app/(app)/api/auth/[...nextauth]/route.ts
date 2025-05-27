6533a6bd31c451cfa726d7f810af496247aa4c99
Sammy <sammy.adjones@gmail.com>
2025-05-26 21:05:49 -0400

    user stays signed in properly now

--- app/(app)/api/auth/[...nextauth]/route.ts
+++ app/(app)/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,18 @@
 import NextAuth from "next-auth";
-// import type { AuthOptions } from "next-auth";
-// import GoogleProvider from "next-auth/providers/google";
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
 import { authOptions } from "@/app/(app)/lib/auth";
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }
+
+export default NextAuth ({ 
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+        }),
+    ],
+    secret: process.env.NEXTAUTH_SECRET,
+});
